ecc3695935345ceb37a7d5cf7f0a1946a17f538e
Heyner Leiva <heynerleiva96@gmail.com>
2024-06-27 12:14:52 -0600

    Se agrega la subida de single/multiple archivos y tanbien la descarga del mismo.

---
+++ src/presentation/file-upload/routes.ts
@@ -0,0 +1,30 @@
+
+import { Router } from 'express';
+import { FileUploadController } from './controller';
+import { FileUploadService } from '../services/file-upload.service';
+import { FileUploadMiddleware } from '../middlewares/file-upload.middleware';
+import { TypeMiddleware } from '../middlewares/type.middleware';
+
+
+export class FileUploadRoutes {
+
+
+  static get routes(): Router {
+
+    const router = Router();
+
+    const controller = new FileUploadController(new FileUploadService());
+
+    router.use(FileUploadMiddleware.containFiles);
+    router.use(TypeMiddleware.validTypes(['users','products','categories']));
+    
+    // Definir las rutas
+    router.post('/single/:type', controller.uploadFile);
+    router.post('/multiple/:type',controller.uploadMultipleFiles);
+
+    return router;
+  }
+
+
+}
+
